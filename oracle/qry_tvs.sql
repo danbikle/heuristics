7364b44b72172c13f73f1207c801c180cf3aadda
Dan Bikle <dan@bot4.us>
2013-12-03 05:14:32 +0000

    oracle-scripts

--- oracle/qry_tvs.sql
+++ oracle/qry_tvs.sql
@@ -33,6 +33,114 @@ tkr
 ,AVG(ng5)/STDDEV(ng4) sharpe_r4
 ,AVG(ng5)/STDDEV(ng5) sharpe_r5
 FROM tvsource
+WHERE ma03s_lt = 1
+GROUP BY tkr
+HAVING COUNT(tkr) > 5
+ORDER BY AVG(ng3)
+;
+
+SELECT
+COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma03s_lt = 1
+;
+
+SELECT
+tkr
+,COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma04s_lt = 1
+GROUP BY tkr
+HAVING COUNT(tkr) > 5
+ORDER BY AVG(ng3)
+;
+
+SELECT
+COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma04s_lt = 1
+;
+
+SELECT
+tkr
+,COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma05s_lt = 1
+GROUP BY tkr
+HAVING COUNT(tkr) > 5
+ORDER BY AVG(ng3)
+;
+
+SELECT
+COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma05s_lt = 1
+;
+
+SELECT
+tkr
+,COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
 WHERE ma09s_lt = 1
 GROUP BY tkr
 HAVING COUNT(tkr) > 5
@@ -53,6 +161,42 @@ COUNT(tkr) ccount
 ,AVG(ng5)/STDDEV(ng5) sharpe_r5
 FROM tvsource
 WHERE ma09s_lt = 1
+;
+
+SELECT
+tkr
+,COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma20s_lt = 1
+GROUP BY tkr
+HAVING COUNT(tkr) > 5
+ORDER BY AVG(ng3)
+;
+
+SELECT
+COUNT(tkr) ccount
+,AVG(ng1) avg_ng1
+,AVG(ng2) avg_ng2
+,AVG(ng3) avg_ng3
+,AVG(ng4) avg_ng4
+,AVG(ng5) avg_ng5
+,AVG(ng5)/STDDEV(ng1) sharpe_r1
+,AVG(ng5)/STDDEV(ng2) sharpe_r2
+,AVG(ng5)/STDDEV(ng3) sharpe_r3
+,AVG(ng5)/STDDEV(ng4) sharpe_r4
+,AVG(ng5)/STDDEV(ng5) sharpe_r5
+FROM tvsource
+WHERE ma20s_lt = 1
 ;
 
 exit
